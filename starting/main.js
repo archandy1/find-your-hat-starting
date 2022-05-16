6fad9e15a52b72a8edf555b251e4ff64bed3c2a8
archandy1 <a.pleczkan@gmail.com>
2022-05-16 20:09:22 +0200

    first commit

---
+++ starting/main.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+    constructor(field =[[]]) {
+        this.field = field;
+
+    }
+
+    print() {
+        
+    }
+
+
+
+}
+
